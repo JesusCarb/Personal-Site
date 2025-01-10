cef7ce340454b9b3d998672701a1d20e3d5547b0
Jesus <je558107@ucf.edu>
2025-01-10 14:14:12 -0500

    testing changes

--- src/App.jsx
+++ src/App.jsx
@@ -22,6 +22,7 @@ function App() {
 
   return (
     <>
+      <h>TESTING</h>
       <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
